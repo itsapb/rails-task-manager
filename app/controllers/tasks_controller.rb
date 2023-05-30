a185e1c874e20c5b171ff6069371276c6af714a5
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-05-30 14:21:03 +0100

    create, read, update completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,30 @@ class TasksController < ApplicationController
   def show
     @tasks = Task.find(params[:id])
   end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+    @tasks.save!
+    redirect_to tasks_path
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+    @tasks.update(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#list'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/new', to: 'tasks#new', as: 'new'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch '/tasks/:id', to: 'tasks#update'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   # Defines the root path route ("/")
   # root "articles#index"
