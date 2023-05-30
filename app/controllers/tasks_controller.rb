21a8e93eb847da3d13c63fa1e8075cd5e79e5c95
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-05-30 13:11:22 +0100

    Can view details of task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,8 @@ class TasksController < ApplicationController
   def list
     @tasks = Task.all
   end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'list', to: 'tasks#list'
+  get '/tasks', to: 'tasks#list'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
   # Defines the root path route ("/")
   # root "articles#index"
 end
